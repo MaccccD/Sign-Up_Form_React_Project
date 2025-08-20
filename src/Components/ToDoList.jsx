4d6cac737b50dbde247e115077dd742c560eea8c
MaccccD <Dumisanimofolo@gmail.com>
2025-08-20 23:36:57 +0200

    Tasks addition update

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -9,6 +9,7 @@ function ToDoList() {
     function AddTasks(){
       //add tasks typed into the holder
       setTasksHolder(...tasks); // in react, we dont use push like in pur js , we add something to an array in this way by using the spread operator
+      setComplete(false);
       console.log("a new task has been added to the tasks holder");
       console.log(tasksHolder);
     }
