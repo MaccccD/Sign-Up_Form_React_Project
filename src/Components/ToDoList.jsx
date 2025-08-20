848f770d430081e95e726ce7eab208147435df01
MaccccD <Dumisanimofolo@gmail.com>
2025-08-21 00:16:21 +0200

    To Do List Update Part 3

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -4,19 +4,13 @@ function ToDoList() {
     //setting up the states :
     const [tasks, setTasks] = useState("");
     const [tasksHolder, setTasksHolder] = useState([]);
-    const [complete, setComplete] = useState(false);
+    const [completed, setCompleted] = useState(false);
 
     function AddTasks(){
       //add tasks typed into the holder
-      // let tasksAdded = [
-      //   tasks = "",
-      //   complete = false,
-      //   Date.toString()
-
-      // ];
       setTasksHolder([...tasksHolder, tasks]); // in react, we dont use push like in pure js , we add something to an array in this way by using the spread operator
       //also you need to add the tasks into the task holder by doing it like above. not just a'task' with no holder like i had before: "...task"(that's wrong)
-      setComplete(false);
+      setCompleted(false);
       console.log("a new task has been added to the tasks holder");
       console.log(tasksHolder);
     }
@@ -24,6 +18,9 @@ function ToDoList() {
    
   return (
     <div>
+      <h1>Welcome to your Daily To-Do List Task Planner.</h1>
+      <p>In here, you'll be able to add tasks that you need to complete and tick them off when they are complete.</p>
+      <br/>
       <input type='text' placeholder='Enter your tasks' onChange={(t)=> setTasks(t.target.value)} />
       <br/>
       <button onClick={AddTasks} className='tasks'>Add Tasks</button>
@@ -33,7 +30,7 @@ function ToDoList() {
         return (
         <li key={index}>
           {task}
-          <input type='checkbox'/>
+          <input type='checkbox' className='checkbox' checked = {task.completed}/>
         </li>
         );
        
