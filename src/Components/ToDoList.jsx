5d9a6c496bd5d2291c828fa8f389bc34f4864161
MaccccD <Dumisanimofolo@gmail.com>
2025-08-20 23:52:42 +0200

    Tasks Holder logic update fix

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -8,7 +8,14 @@ function ToDoList() {
 
     function AddTasks(){
       //add tasks typed into the holder
-      setTasksHolder(...tasks); // in react, we dont use push like in pur js , we add something to an array in this way by using the spread operator
+      // let tasksAdded = [
+      //   tasks = "",
+      //   complete = false,
+      //   Date.toString()
+
+      // ];
+      setTasksHolder([...tasksHolder, tasks]); // in react, we dont use push like in pure js , we add something to an array in this way by using the spread operator
+      //also you need to add the tasks into the task holder by doing it like above. not just a'task' with no holder like i had before: "...task"(that's wrong)
       setComplete(false);
       console.log("a new task has been added to the tasks holder");
       console.log(tasksHolder);
@@ -18,8 +25,8 @@ function ToDoList() {
   return (
     <div>
       <input type='text' placeholder='Enter your tasks' onChange={(t)=> setTasks(t.target.value)} />
-      <br/><br/>
-      <button onClick={AddTasks}>Add Tasks</button>
+      <br/>
+      <button onClick={AddTasks} className='tasks'>Add Tasks</button>
       <br/>
      <ul>
       {tasksHolder.map((task, index)=>{
