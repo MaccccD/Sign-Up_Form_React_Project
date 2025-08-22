46bf63d3e8b561bc236e64d9f27ba198531a9156
MaccccD <Dumisanimofolo@gmail.com>
2025-08-22 21:53:47 +0200

    Clearing existing tasks Update
    -don't forget the "value" prop that needs to go in the input together with the onChange method.

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -11,12 +11,12 @@ function ToDoList() {
       setTasksHolder([...tasksHolder, tasks]); // in react, we dont use push like in pure js , we add something to an array in this way by using the spread operator
       //also you need to add the tasks into the task holder by doing it like above. not just a'task' with no holder like i had before: "...task"(that's wrong)
       setCompleted(false);
-      setTasks(""); // clear tasks
+      //tasks.value = "";
+      setTasks(""); // clear tasks. Need to add the value prop so that it connects the tasks to this as well
       setCompleted(false);
       console.log("a new task has been added to the tasks holder");
       console.log(tasksHolder);
     }
-
 
     const removeTasks = (itemToRemove)=> {
       if(tasksHolder.length === 0){ // instead of using includes , you need to check if the holder is emepty first
@@ -42,7 +42,7 @@ function ToDoList() {
       <h1>Welcome to your Daily To-Do List Task Planner.</h1>
       <p>In here, you'll be able to add tasks that you need to complete and tick them off when they are complete.</p>
       <br/>
-      <input type='text' placeholder='Enter your tasks' onChange={(t)=> setTasks(t.target.value)} />
+      <input type='text' placeholder='Enter your tasks' value={tasks} onChange={(t)=> setTasks(t.target.value)} /> 
       <br/>
       <button onClick={AddTasks} className='tasks'>Add Tasks</button>
       <br/>
