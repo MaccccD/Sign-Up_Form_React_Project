46e228d373b49ce133023cfad7e517f5abb842ef
MaccccD <Dumisanimofolo@gmail.com>
2025-08-25 23:26:30 +0200

    Task Completion logic Update
    -added the task completion logic.
    -struggle: i was passing a function in the "checked" prop of the input instead of a boolean value. Also was using normal adding something to array logic instead of object de-structuring.
    -Fixes: using object de-structuring for the tasks as well as fixing the completed task logic

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -4,24 +4,30 @@ function ToDoList() {
     //setting up the states :
     const [tasks, setTasks] = useState("");
     const [tasksHolder, setTasksHolder] = useState([]);
-    const [completed, setCompleted] = useState(false);
+   
 
     function AddTasks(){
       //add tasks typed into the holder
-      setTasksHolder([...tasksHolder, tasks]); // in react, we dont use push like in pure js , we add something to an array in this way by using the spread operator
-      //also you need to add the tasks into the task holder by doing it like above. not just a'task' with no holder like i had before: "...task"(that's wrong)
-      setCompleted(false);
+      const newTask = { // changed to this to object destructruring . Kinda like an Enum but in js
+        id: Date.now(),
+        text: tasks,
+        completed: false
+      }
+      setTasksHolder([...tasksHolder, newTask]); // in react, we dont use push like in pure js , we add something to an array in this way by using the spread operator
+      //also you need to add the tasks into the task holder by doing it like above. not just a'task' with no holder like i had before: "...task"(that's wrong
       //tasks.value = "";
       setTasks(""); // clear tasks. Need to add the value prop so that it connects the tasks to this as well
-      setCompleted(false);
       console.log("a new task has been added to the tasks holder");
       console.log(tasksHolder);
+      if(tasksHolder != null){
+        alert("Please add task first !");
+        return;
+      }
     }
 
     const removeTasks = (itemToRemove)=> {
       if(tasksHolder.length === 0){ // instead of using includes , you need to check if the holder is emepty first
          alert("No tasks have been added yet so there is nothing to delete");
-         setCompleted(false);
          return;
       }
       let updatedTasks = tasksHolder.filter(item => item !== itemToRemove); // to remove something n an anraay on react , we use "filter" instea dof normal js methods such as "pop" or "slice"
@@ -36,12 +42,14 @@ function ToDoList() {
       }
     };
 
-    const completeTasks = (taskChecked) =>{
-      if(completed === taskChecked){
-        setCompleted(true);
-        alert("This task has just been completed.")
-        return;
+    const ToggleTaskCompletion = (taskChecked) =>{ // asked for help but the reason why it wasn't working was bc i was cllaing the toggle fucntion isntead of passing the bollean value in the "checked" propertyy
+     const updatedTasks = tasksHolder.map((task)=>{
+      if(task.id === taskChecked){
+        return {...task, completed: !task.completed}
       }
+      return task;
+     });
+     setTasksHolder(updatedTasks);
     }
     
    
@@ -50,21 +58,24 @@ function ToDoList() {
       <h1>Welcome to your Daily To-Do List Task Planner.</h1>
       <p>In here, you'll be able to add tasks that you need to complete and tick them off when they are complete.</p>
       <br/>
-      <input type='text' placeholder='Enter your tasks' value={tasks} onChange={(t)=> setTasks(t.target.value)} /> 
+      <input type='text' placeholder='Enter your tasks' value={tasks} onChange={(t)=> setTasks(t.target.value)} onKeyDown={(t)=> t.key === 'Enter' && AddTasks} /> 
       <br/>
       <button onClick={AddTasks} className='tasks'>Add Tasks</button>
       <br/>
-      <br/>
       <button onClick={removeTasks} className='tasks'>Delete Task</button>
      <ul>
-      {tasksHolder.map((task, index)=>{
+      <h1>Your Tasks: ({tasksHolder.length}):</h1>
+      <br/>
+      {tasksHolder.map((task)=>{
         return (
-        <li key={index}>
-          {task}
-          <input type='checkbox' className='checkbox' checked= {completeTasks}/>
+        <li key={task.id}>
+          {task.text}
+           <input type='checkbox' 
+           className='checkbox' 
+           checked= {task.completed} 
+           onChange={()=>ToggleTaskCompletion(task.id)}/>
         </li>
         );
-       
       })}
      </ul>
     </div>
